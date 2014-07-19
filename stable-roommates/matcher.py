d1f49ec88ad47f4efa75fece7586f789e4c2e268
charlierproctor <charlie.proctor@yahoo.com>
2014-07-19 15:01:42 -0400

    setup phase complete. person object defined, hash setup, etc...starting work on phase 1

--- stable-roommates/matcher.py
+++ stable-roommates/matcher.py
@@ -1,149 +1,78 @@
-# Stable Marriage Algorithm
+# Stable Roommate Algorithm
 
-guyprefers = {
- 'abe':  ['abi', 'eve', 'cath', 'ivy', 'jan', 'dee', 'fay', 'bea', 'hope', 'gay'],
- 'bob':  ['cath', 'hope', 'abi', 'dee', 'eve', 'fay', 'bea', 'jan', 'ivy', 'gay'],
- 'col':  ['hope', 'eve', 'abi', 'dee', 'bea', 'fay', 'ivy', 'gay', 'cath', 'jan'],
- 'dan':  ['ivy', 'fay', 'dee', 'gay', 'hope', 'eve', 'jan', 'bea', 'cath', 'abi'],
- 'ed':   ['jan', 'dee', 'bea', 'cath', 'fay', 'eve', 'abi', 'ivy', 'hope', 'gay'],
- 'fred': ['bea', 'abi', 'dee', 'gay', 'eve', 'ivy', 'cath', 'jan', 'hope', 'fay'],
- 'gav':  ['gay', 'eve', 'ivy', 'bea', 'cath', 'abi', 'dee', 'hope', 'jan', 'fay'],
- 'hal':  ['abi', 'eve', 'hope', 'fay', 'ivy', 'cath', 'jan', 'bea', 'gay', 'dee'],
- 'ian':  ['hope', 'cath', 'dee', 'gay', 'bea', 'abi', 'fay', 'ivy', 'jan', 'eve'],
- 'jon':  ['abi', 'fay', 'jan', 'gay', 'eve', 'bea', 'dee', 'cath', 'ivy', 'hope']}
-galprefers = {
- 'abi':  ['bob', 'fred', 'jon', 'gav', 'ian', 'abe', 'dan', 'ed', 'col', 'hal'],
- 'bea':  ['bob', 'abe', 'col', 'fred', 'gav', 'dan', 'ian', 'ed', 'jon', 'hal'],
- 'cath': ['fred', 'bob', 'ed', 'gav', 'hal', 'col', 'ian', 'abe', 'dan', 'jon'],
- 'dee':  ['fred', 'jon', 'col', 'abe', 'ian', 'hal', 'gav', 'dan', 'bob', 'ed'],
- 'eve':  ['jon', 'hal', 'fred', 'dan', 'abe', 'gav', 'col', 'ed', 'ian', 'bob'],
- 'fay':  ['bob', 'abe', 'ed', 'ian', 'jon', 'dan', 'fred', 'gav', 'col', 'hal'],
- 'gay':  ['jon', 'gav', 'hal', 'fred', 'bob', 'abe', 'col', 'ed', 'dan', 'ian'],
- 'hope': ['gav', 'jon', 'bob', 'abe', 'ian', 'dan', 'hal', 'ed', 'col', 'fred'],
- 'ivy':  ['ian', 'col', 'hal', 'gav', 'fred', 'bob', 'abe', 'ed', 'jon', 'dan'],
- 'jan':  ['ed', 'hal', 'gav', 'abe', 'bob', 'jon', 'col', 'ian', 'fred', 'dan']}
- 
+prefs = {
+'1': ['3','4','2','6','5'],
+'2': ['6','5','4','1','3'],
+'3': ['2','4','5','1','6'],
+'4': ['5','2','3','6','1'],
+'5': ['3','1','2','4','6'],
+'6': ['5','1','3','4','2']}
+
+
 class Person:
 	def __init__(self, name):
 		self.name = name
-		self.fiance = None
 		self.preferences = []
-		self.proposals = []
 
-	def __str__(self):
-		return self.name
+	# class variable to store all the people!
+	ppl = {}
 
-	def free(self):
-		self.fiance = None
+	#somebody is going to propose!!
+	def propose_to(self,somebody):
+		somebody.receive_proposal_from(self)
 
-	def single(self):
-		return self.fiance == None
+	# when somebody receives a proposal in phase 1
+	def receive_proposal_from(self,somebody):
+		current_prefs = self.preferences
 
-	#engage somebody
-	def engage(self, person):
-		self.fiance = person
-		person.fiance = self
+		# need to cross off those behind current proposal
+		prefs_to_chop = current_prefs[(current_prefs.index(somebody)+1):]  
 
-	#determine whether the person is a better choice (higher in the list)
-	def better_choice(self, person):
-		print(self.name + " is evaluating her options...")
-		if self.preferences.index(person) < self.preferences.index(self.fiance):
-			print(self.name + " decides " + person.name + " is a better choice than current fiance " + self.fiance.name)
-		else:
-			print(self.name + " decides " + person.name + " is a worse choice than current fiance " + self.fiance.name)
-		# print(self.preferences.index(person) < self.preferences.index(self.fiance))
-		return self.preferences.index(person) < self.preferences.index(self.fiance)
-  
-  	#propose to somebody
-	def propose_to(self,person):
-		self.proposals.append(person)
-		person.proposal_from(self)
+		#use the cross_off method to cross off these matches...
+		for person in prefs_to_chop:
+			self.cross_off(person)
 
-	#receive a proposal from the opposite sex
-	def proposal_from(self, person):
-		#if single, accept!
-		if self.single():
-			self.engage(person)
-			print(self.name + " is single and accepts " + person.name + "'s proposal")
-		#if the proposer is a better choice, accept!
-		elif self.better_choice(person):
-			print(self.name + " is dumping " + self.fiance.name + " for " + person.name)
-			self.fiance.free()
-			self.engage(person)
-		#otherwise, reject!
-		else:
-			print(self.name + " rejects " + person.name + " to keep " + self.fiance.name)
+	# crosses off a potential match
+	def cross_off(self,person):
+		#removes each from each other's preference array
+		if person.name in self.preferences:
+			self.preferences.remove(person.name)
+		if self.name in person.preferences:
+			person.preferences.remove(self.name)
 
+	#return just the names of the people in the preferences
+	def getPrefs(self):
+		res = []
+		for pref in self.preferences:
+			res.append(pref.name)
+		return res
 
-# create men, women hashes which have names as keys
+	# generates the hash to display all people's current preferences
+	def prefsMatrix():
+		res = {}
+		for person_name,person_object in Person.ppl.items():
+			res[person_name] = person_object.getPrefs()
+		return res
+
+# SETUP --> CREATE PERSON OBJECTS
+
+# create ppl hash which have names as keys
 # and the corresponding person objects as values
-men = {}
-for guy in guyprefers:
-	men[guy] = Person(guy)
+for person in prefs:
+	Person.ppl[person] = Person(person)
 
-women = {}
-for gal in galprefers:
-	women[gal] = Person(gal)
+# add the appropriate person objects to their preference array
+for person_name,person_object in Person.ppl.items():   # the person
+	for pref_name in prefs[person_name]:      #their preferences
+		person_object.preferences.append(Person.ppl[pref_name])
 
-# for all men, add the appropriate women objects to
-# their preference arrays
-for man_name,man_object in men.items():
-	for woman_name in guyprefers[man_name]:
-		man_object.preferences.append(women[woman_name])
+# PHASE 1
 
-# for all women...
-for woman_name,woman_object in women.items():
-	for man_name in galprefers[woman_name]:
-		woman_object.preferences.append(men[man_name])
-
-#find all the single men
-def find_single(gender):
-	print("Looking for single men...")
-	for person,person_object in gender.items():
-		if person_object.single():
-			print(person_object.name + " is single")
-			return person_object
-	print("Nobody is single...")
-	return False
-
-#find the man's next top choice (whom he has yet to propose to)
-def find_next_best(man,women):
-	for woman in man.preferences:
-		if not (woman in man.proposals):
-			print(man.name + "'s next choice is " + woman.name)
-			return woman
-
-#method to free everybody of their spouse
-def free_everybody():
-	for man in men:
-		men[man].free()
-	for woman in women:
-		women[woman].free()
-
-#core matching method
-def match(men,women):
-	free_everybody()
-
-	done = False
-
-	while not done:
-		#find the single men
-		man = find_single(men)
-		if not man:
-			#if there aren't any, we're done
-			done = True
-		else:
-			#if there are, find the next best woman, and propose to her
-			woman = find_next_best(man,women)
-			print(man.name + " is proposing to " + woman.name)
-			man.propose_to(woman)
-
-#perform the match
-match(men,women)
-
-#print the results
-print("\nTHE RESULTS\n")
-for man_name,man_object in men.items():
-	print(man_name + ", " + man_object.fiance.name)
+# for person in Person.ppl.values():
+# 	person.propose_to(person.preferences[0])
 
 
+
+
+# SEE THE RESULTS
+print(Person.prefsMatrix())
