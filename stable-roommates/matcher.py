9ebbe3034b9d5666b154db36fb7380beb9518d88
charlierproctor <charlie.proctor@yahoo.com>
2014-07-20 20:46:28 -0400

    added initial_prefs attribute to person class to track initial preferences; this will remain unchanged!; renamed self.preferences to self.current_prefs; refactored prefsMatrix() method

--- stable-roommates/matcher.py
+++ stable-roommates/matcher.py
@@ -13,15 +13,16 @@ sys.setrecursionlimit(10000)
 # prefs = Tester.rosetta_marriage
 
 # random preference array generator in tester.py
-prefs = Tester.random_matrix(101)
+prefs = Tester.random_matrix(12)
 
 
 class Person:
 	def __init__(self, name):
 		self.name = name
-		self.preferences = []
+		self.initial_prefs = []   # a person's initial set of preferences
+		self.current_prefs = []     # a person's current set of preferences
 
-	# class variable to store all the people!
+	# class variable to store all the people
 	ppl = {}
 
 	#somebody is going to propose!!
@@ -30,11 +31,12 @@ class Person:
 
 	# when somebody receives a proposal in phase 1
 	def receive_proposal_from(self,somebody):
-		current_prefs = self.preferences
+		current_prefs = self.current_prefs
 
 		# need to cross off those behind current proposal
 		prefs_to_chop = current_prefs[(current_prefs.index(somebody)+1):]  
-		# this is what causes a rotation to halt!! --> ie, we have reached another stable table
+		# this is what causes a rotation to halt!! 
+			# --> ie, we have reached another stable table
 		# when there is nobody left to cross off.
 
 		#use the cross_off method to cross off these matches...
@@ -44,27 +46,33 @@ class Person:
 	# crosses off a potential match
 	def cross_off(self,person):
 		#removes each from each other's preference array
-		if person in self.preferences:       
-			self.preferences.remove(person)
-		if self in person.preferences:
-			person.preferences.remove(self)
+		if person in self.current_prefs:       
+			self.current_prefs.remove(person)
+		if self in person.current_prefs:
+			person.current_prefs.remove(self)
 
 		#initiate a new proposal
-		if len(person.preferences) > 0:
-			person.propose_to(person.preferences[0])
+		if len(person.current_prefs) > 0:
+			person.propose_to(person.current_prefs[0])     
+		# why do they always propose to their top choice??  
+		# (even if we haven't crossed off their old top choice?)
 
-	#return just the names of the people in the preferences
-	def getPrefs(self):
+	#return just the names of the people in the preference arrays
+	def getPrefs(self,time):
 		res = []
-		for pref in self.preferences:
-			res.append(pref.name)
+		if time == 'initial':
+			for pref in self.initial_prefs:
+				res.append(pref.name)
+		else:
+			for pref in self.current_prefs:
+				res.append(pref.name)
 		return res
 
-	# generates the hash to display all people's current preferences
-	def prefsMatrix():
+	# generates the hash to display all people's preferences
+	def prefsMatrix(time):
 		res = {}
 		for person_name,person_object in Person.ppl.items():
-			res[person_name] = person_object.getPrefs()
+			res[person_name] = person_object.getPrefs(time)
 		return res
 
 # SETUP --> CREATE PERSON OBJECTS
@@ -76,11 +84,14 @@ for person in prefs:
 
 # add the appropriate person objects to their preference array
 for person_name,person_object in Person.ppl.items():   # the person
-	for pref_name in prefs[person_name]:      #their preferences
-		person_object.preferences.append(Person.ppl[pref_name])
+	for pref_name in prefs[person_name]:      #their current_prefs
+		# populate the initial preference array
+		person_object.initial_prefs.append(Person.ppl[pref_name])
+		# populate the current preference array
+		person_object.current_prefs.append(Person.ppl[pref_name])
 
 print("Starting Preference Matrix -- Phase 0 Complete:")
-pprint.pprint(Person.prefsMatrix())
+pprint.pprint(Person.prefsMatrix('current'))
 
 # PHASE 1
 
@@ -88,11 +99,11 @@ pprint.pprint(Person.prefsMatrix())
 for person in Person.ppl.values():
 	# before proposing, we need to make sure everybody hasn't rejected them yet!
 	# this would happen iff everybody else has already received better offers...
-	if len(person.preferences) > 0:       
-		person.propose_to(person.preferences[0])  # propose to your top choice
+	if len(person.current_prefs) > 0:       
+		person.propose_to(person.current_prefs[0])  # propose to your top choice
 
 print("Phase 1 Complete:")
-pprint.pprint(Person.prefsMatrix())
+pprint.pprint(Person.prefsMatrix('current'))
 
 
 # PHASE 2
@@ -102,12 +113,13 @@ pprint.pprint(Person.prefsMatrix())
 def find_person_with_second_column():
 	res = False
 	for person in Person.ppl.values():
-		if len(person.preferences) > 1:
+		if len(person.current_prefs) > 1:
 			res = person
 			break
 	return res
 
-current_person = find_person_with_second_column()    #find the initial person with a second column
+#find the initial person with a second column
+current_person = find_person_with_second_column()    
 
 num_rotations = 1    # number of rotations so far...
 
@@ -115,22 +127,30 @@ num_rotations = 1    # number of rotations so far...
 while current_person: 
 
 	# grab their second choice preference
-	current_pref = current_person.preferences[1]
+	current_pref = current_person.current_prefs[1]
 
-	# and cross off that person's last preference.... which kicks off the rotation
-	current_pref.cross_off(current_pref.preferences[-1])
+	# and cross off that person's last preference.... 
+	# which kicks off the rotation
+	current_pref.cross_off(current_pref.current_prefs[-1])
 
-	print("Rotating around person " + current_person.name + ", with preference " + current_pref.name)
+	print("Rotating around person " + current_person.name + 
+		", with preference " + current_pref.name)
 	print("Rotation #" + str(num_rotations) + ":")
 
 	num_rotations = num_rotations + 1
-	pprint.pprint(Person.prefsMatrix())	
+	pprint.pprint(Person.prefsMatrix('current'))	
 
 	# find another person to work with... if there is one
 	current_person = find_person_with_second_column()
 
 
 print("Phase 2 Complete:")
-pprint.pprint(Person.prefsMatrix())
+pprint.pprint(Person.prefsMatrix('current'))
+
+# pprint.pprint(Person.prefsMatrix('initial'))
+# def stable_match():
+	# for person_name,person_object in Person.ppl:
+
+
 
 
